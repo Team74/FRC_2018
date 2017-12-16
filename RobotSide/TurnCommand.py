5bb451ffebfc5c87a928ee121e9eb36bf1ec21b2
William Hescott <wihe132@hps21.org>
2017-12-16 00:01:12 -0500

    Oh, I did say I was going to work over the week, didn't I...?
    -Moved TurnCommand into the same folder as my other stuff (I don't know
    how it ended up outside of the folder I created it in but I'm known to
    make a mistake or two on occasion)
    -Figure out how the final file structure will look
    -Create code to test new changes
    -Can we mine Bitcoin on the roboRio?1?!?!?!?!!!!!??
    -Commented out old code in autonhandling and replaced it with some
    pseudo-code that should work with Jacob's system
    TODO:
    -I don't know how to import Jacobs file to my program. It's not defined
    as a class so as far as I can tell it's basically just going to be a
    script. I don't know how to get info from one of those ...

--- RobotSide/TurnCommand.py
+++ RobotSide/TurnCommand.py
@@ -9,6 +9,7 @@ class TurnCommand():
     def __init__(self, speed, angle):
         self.speed = speed
         self.angle=angle
+        
     def runCommand(self):
 
         print ("We're turning " + self.speed + " in direction: " + self.angle)
