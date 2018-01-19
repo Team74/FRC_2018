2d0daa936ebf7a382777fd48b22e20cbc5a178e9
Sam VanderArk <vanderarks@brpsk12.org>
2018-01-18 19:45:29 -0500

    gvscmkllgfvjsdfkop[ guj iwqerp9 qer[-h]

--- GUI/converter.py
+++ GUI/converter.py
@@ -1,6 +1,6 @@
 width = 100		#width in whatever units you want it to be
 height = 100		#height, same
-outputName = None#"test.txt"	#if you want it stored in a file, put it here. leave it as None if you want it to print to console
+outputName = False	#if you want it stored in a file, put it here. leave it as None if you want it to print to console
 
 
 #---------------------------------------------------------------------
@@ -23,15 +23,16 @@ commandlist = []
 
 for line in file:
 	if line[:6] == "Node> ":
-		x = line[6:].split(", ")
+		x = line[6:-1].split(", ")
 		temp = [float(x[0].split(":")[1])*width, float(x[1].split(":")[1])*height]
 		new_angle = math.degrees(math.atan2(-(temp[0] - nodepos[0]), (temp[1] - nodepos[1])))
 		commandlist.append("[1," + str((angle - new_angle + 180) % 360 - 180) + "]") #assuming the robot points forward on the field, do -x, y for y,x to account for 90 degree rotation
-		commandlist.append("[0," + str(math.sqrt((nodepos[0] - temp[0])**2 + (nodepos[1] - temp[1])**2)) + "]")
+		dist = math.sqrt((nodepos[0] - temp[0])**2 + (nodepos[1] - temp[1])**2)
+		commandlist.append("[0,1," + str(dist) + ",1," + str(dist) + "]")
 		nodepos = temp
 		angle = new_angle
 	elif line[:6] == "Comm> ":
-		commandlist.append("[" + commandOptions[line[6:]] + "]")
+		commandlist.append("[" + commandOptions[line[6:-1]] + "]")
 	else:
 		print("Type Not Found")
 
@@ -39,10 +40,10 @@ outputstring = ""
 
 for i in commandlist:
 	outputstring += (i + "\n")
-if outputName is None:
+if outputName:
 	print(outputstring)
 else:
-	zing = open("convert/" + outputName, "w" )
+	zing = open("convert/" + filename, "w" )
 	zing.write(outputstring)
 
 
