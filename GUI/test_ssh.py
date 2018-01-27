48fe92ac49c7e76a84e5885a2609fad691754e1e
Sam VanderArk <vanderarks@brpsk12.org>
2018-01-27 13:01:36 -0500

    holy geez we've got ssh now guys

--- GUI/test_ssh.py
+++ GUI/test_ssh.py
@@ -8,6 +8,8 @@ import os
 from os import listdir
 from os.path import (basename, getsize, isdir)
 
+import stat
+
 '''have_win32file = False
 if platform == 'win':
     try:
@@ -32,61 +34,29 @@ class FileSystemOverSSH(FileSystemAbstract):
     def __init__(self, target, username, password):
         super().__init__()
 
-        #self.client = paramiko.SSHClient()
-        #self.client.load_system_host_keys()
-        #self.client.connect(target, username=username, password=password)
-        host_keys = paramiko.util.load_host_keys(os.path.expanduser('~/.ssh/known_hosts'))
-        t = paramiko.Transport((target, 22))
-        t.connect(host_keys[target][host_keys[target].keys()[0]], username, password, )
-        self.sftp = paramiko.SFTPClient.from_transport(t)
-
+        self.client = paramiko.SSHClient()
+        self.client.load_system_host_keys()
+        self.client.connect(target, username=username, password=password)
+        self.sftp = self.client.open_sftp()
 
     def __del__(self):
-        #self.sftp.close()
         self.sftp.close()
-        print("end my life fam")
+        self.client.close()
 
     def listdir(self, fn):
-        #print(fn)
-        #stdin, stdout, stderr = self.client.exec_command('python -c "from os import listdir; import sys; print(listdir(sys.argv[1]))" "' + fn[1:] + '"')
-        #x = list(set(map(lambda x: x[:-1], [x for x in stdout])))
-        #print(x)
-        #print("\n\n")
-        #s = self.client.open_sftp()
-        x = self.sftp.listdir(fn)
-        #s.close() #self.sftp.
-        return x
+        return self.sftp.listdir(fn)
 
     def getsize(self, fn):
-        #print(fn[1:])
-        #stdin, stdout, stderr = self.client.exec_command('python -c "from os.path import getsize; import sys; print(getsize(sys.argv[1]))" ' + fn[1:])
-        #print("sjdklfjsldk")
-        #print([x for x in stdout])
-        #print("fjsdklfjl\n\n")
-        #s = self.client.open_sftp()
-        x = self.sftp.stat(fn).st_size #getsize(fn)
-        #s.close()
-        return x
+        return self.sftp.stat(fn).st_size
 
     def is_hidden(self, fn):
-        return False
-        '''if platform == 'win':
-            if not _have_win32file:
-                return False
-            try:
-                return GetFileAttributesExW(fn)[0] & FILE_ATTRIBUTE_HIDDEN
-            except error:
-                # This error can occurred when a file is already accessed by
-                # someone else. So don't return to True, because we have lot
-                # of chances to not being able to do anything with it.
-                Logger.exception('unable to access to <%s>' % fn)
-                return True
-
-        return basename(fn).startswith('.')'''
+        #return False
+        #assuming the robot runs linux not windows.
+        return basename(fn).startswith('.')
 
     def is_dir(self, fn):
-        #stdin, stdout, stderr = self.client.exec_command('python -c "from os.path import isdir; import sys; print(isdir(sys.argv[1]))" "' + fn[1:] + '"')
-        return False#list(map(lambda x: x[:-1], [x for x in stdout]))[-1] == "True"
+        return stat.S_ISDIR(self.sftp.stat(fn).st_mode)
+
 
 if __name__ == '__main__':
     #comm = open("commands.dat", "r")
