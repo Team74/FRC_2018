5e006d6b30c1e6872dd42cf8fba91a851d3ce3bf
Karthik Karyamapudi <karthikkaryamapudi@gmail.com>
2018-02-17 15:13:56 -0500

    Auton Tuning

--- Comp_Bot_Code/autonNearScale.py
+++ Comp_Bot_Code/autonNearScale.py
@@ -4,23 +4,19 @@ from autonBaseInit import *
 
 class autonNearScale(autonBaseInit):
     def run(self):
-        if self.position == 'L' and self.side == 'L':
-            if self.drive.autonMove(1, 0, .5, 180, 0, 0, 0, 0):
+        if self.side == 'left' and self.scalePosition == 'L':
+            if self.drive.autonMove(1, 0, 1, 200, 0, 0, 0, 0):
                 pass
-            elif self.drive.autonMove(2, 1, 0, 0, 90, .3, 0, 0):
+            elif self.drive.autonMove(2, 1, 0, 0, 20, .5, 0, 0):
                 pass
-            elif self.drive.autonMove(3, 0, .5, 20, 0, 0, 0, 0):
+            elif self.drive.autonMove(3, 0, .75, 70, 0, 0, 0, 0):
                 pass
-            elif self.drive.autonMove(4, 2, 0, 0, 0, 0, 0, 0):
+        if self.side == 'right' and self.scalePosition == 'R':
+            if self.drive.autonMove(1, 0, 1, 200, 0, 0, 0, 0):
                 pass
-        elif self.position == 'R' and self.side == 'R':
-            if self.drive.autonMove(1, 0, .5, 180, 0, 0, 0, 0):
+            elif self.drive.autonMove(2, 1, 0, 0, -20, .5, 0, 0):
                 pass
-            elif self.drive.autonMove(2, 1, 0, 0, -90, .3, 0, 0):
-                pass
-            elif self.drive.autonMove(4, 0, .5, 20, 0, 0, 0, 0):
-                pass
-            elif self.drive.autonMove(4, 2, 0, 0, 0, 0, 0, 0):
+            elif self.drive.autonMove(3, 0, .75, 70, 0, 0, 0, 0):
                 pass
         else:
             pass
