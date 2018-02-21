08af962aafde0b50243caa330b24065196d7586c
Karthik Karyamapudi <karthikkaryamapudi@gmail.com>
2018-02-20 19:44:12 -0500

    Auton Changes

--- Comp_Bot_Code/autonFarScale.py
+++ Comp_Bot_Code/autonFarScale.py
@@ -2,29 +2,29 @@ import wpilib
 from drive import driveTrain
 from autonBaseInit import *
 
-class autonFarSwitch(autonBaseInit):
+class autonFarScale(autonBaseInit):
     def run(self):
         if self.side == 'left' and self.scalePosition == 'R':
-            if self.drive.autonMove(1, 0, 1, 275, 0, 0, 0, 0):
+            if self.drive.autonMove(1, 0, 1, 230, 0, 0, 0, 0):
                 pass
             elif self.drive.autonMove(2, 1, 0, 0, 90, .5, 0, 0):
                 pass
-            elif self.drive.autonMove(3, 0, 1, 600, 0, 0, 0, 0):
+            elif self.drive.autonMove(3, 0, 1, 210, 0, 0, 0, 0):
                 pass
             elif self.drive.autonMove(4, 1, 0, 0, -90, .5, 0, 0):
                 pass
-            elif self.drive.autonMove(5, 0, .5, 20, 0, 0, 0, 0):
+            elif self.drive.autonMove(5, 0, 1, 60, 0, 0, 0, 0):
                 pass
         elif self.side == 'right' and self.scalePosition == 'L':
-            if self.drive.autonMove(1, 0, .5, 60, 0, 0, 0, 0):
+            if self.drive.autonMove(1, 0, 1, 230, 0, 0, 0, 0):
                 pass
             elif self.drive.autonMove(2, 1, 0, 0, -90, .5, 0, 0):
                 pass
-            elif self.drive.autonMove(3, 0, .3, 120, 0, 0, 0, 0):
+            elif self.drive.autonMove(3, 0, 1, 210, 0, 0, 0, 0):
                 pass
             elif self.drive.autonMove(4, 1, 0, 0, 90, .5, 0, 0):
                 pass
-            elif self.drive.autonMove(5, 0, .5, 20, 0, 0, 0, 0):
+            elif self.drive.autonMove(5, 0, 1, 60, 0, 0, 0, 0):
                 pass
         else:
             pass
--- Comp_Bot_Code/autonTwoCubeScale.py
+++ Comp_Bot_Code/autonTwoCubeScale.py
@@ -3,7 +3,7 @@ from drive import driveTrain
 from autonBaseInit import *
 
 class autonTwoCubeScale(autonBaseInit):
-#def autonMove(moveNumberPass, commandNumber, speed, distance, turnAngle):
+#def autonMove(moveNumberPass, commandNumber, speed, distance, turnAngle, turnspeed, liftposition, intakemode):
     def run(self):
         if self.side == 'left' and self.scalePosition == 'L':
             if self.drive.autonMove(1, 0, 1, 250, 0, 0, 0, 0):
@@ -23,6 +23,21 @@ class autonTwoCubeScale(autonBaseInit):
             elif self.drive.autonMove(8, 0, .75, 17.5, 0, 0, 0, 0):
                 pass
         elif self.side == 'right' and self.scalePosition == 'R':
-            pass
+            if self.drive.autonMove(1, 0, 1, 250, 0, 0, 0, 0):
+                pass
+            elif self.drive.autonMove(2, 1, 0, 0, -25, .5, 0, 0):
+                pass
+            elif self.drive.autonMove(3, 0, .75, 20, 0, 0, 0, 0):
+                pass
+            elif self.drive.autonMove(4, 1, 0, 0, -135, .5, 0, 0):
+                pass
+            elif self.drive.autonMove(5, 0, .75, 42, 0, 0, 0, 0):
+                pass
+            elif self.drive.autonMove(6, 0, -.75, 22, 0, 0, 0, 0):
+                pass
+            elif self.drive.autonMove(7, 1, 0, 0, 135, .45, 0, 0):
+                pass
+            elif self.drive.autonMove(8, 0, .75, 17.5, 0, 0, 0, 0):
+                pass
         else:
             pass
