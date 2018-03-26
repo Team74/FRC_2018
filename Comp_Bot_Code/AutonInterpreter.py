f398e093b9c01d820a31e4d038e4d55a68f4dfd1
Sam VanderArk <vanderarks@brpsk12.org>
2018-03-26 19:54:04 -0400

    Added the lift option to the program

--- Comp_Bot_Code/AutonInterpreter.py
+++ Comp_Bot_Code/AutonInterpreter.py
@@ -9,10 +9,13 @@ class AutonInterpreter(autonBaseInit):
 
     def __init__(self,side, switchPosition, scalePosition, driveTrain):#,   name):
         super().__init__(side, switchPosition, scalePosition, driveTrain)
+        #self.drive = "AYYY"
 
-        with open("/home/lvuser/prog_auton.txt") as f:
+        self.lift_pos = 0
+
+        with open("/home/lvuser/prog_auton.txt") as f:	#"/home/svanderark/FRC_2018/GUI/test_prog_auton.txt"
             name = f.readline().strip()
-        print("sfhdjkfhwsdkjfhjskdhfjksdhfkjsd\t" + name)
+        #print("sfhdjkfhwsdkjfhjskdhfjksdhfkjsd\t" + name)
         self.masterlist = []
         self.default_loc = "/home/lvuser/prog_auton_dir/" #"/home/svanderark/FRC_2018/GUI/convert/"
         with open(self.default_loc + name, "r") as f:
@@ -24,15 +27,16 @@ class AutonInterpreter(autonBaseInit):
                 i += 1
                 temp = line[:-1].split(",")
                 if temp[0] == "0":
-                    self.masterlist.append(partial(self.bloob, self.drive, i, 0, float(temp[1]), float(temp[2]), 0, 0))
+                    self.masterlist.append(partial(self.bloob, self.drive, i, 0, float(temp[1]), float(temp[2]), 0, 0, self.lift_pos))
                 elif temp[0] == "1":
                     turnSpeed = 0.5
-                    self.masterlist.append(partial(self.bloob, self.drive, i, 1, 0, 0, float(temp[1]), float(turnSpeed)))
-                else:
-                    self.masterlist.append(partial(self.bloob, self.drive, i, float(temp[0]), 0,0,0,0))
+                    self.masterlist.append(partial(self.bloob, self.drive, i, 1, 0, 0, float(temp[1]), float(turnSpeed), self.lift_pos))
+                elif temp[0] == "2":
+                    self.lift_pos = float(temp[1])
     def run(self):
         for i in self.masterlist:
             i()
 
-    def bloob(self, a, b, c, d, e, f, g, _h=0, _i=0):
-        a.autonMove(b,c,d,e,f,g,0,0)
+    def bloob(self, a, b, c, d, e, f, g, _h=0, _i=0, _j=0):
+        #print(str(a) + "\t" + str(b) + "\t" + str(c) + "\t" + str(d) + "\t" + str(e) + "\t" + str(f) + "\t" + str(g) + "\t" + str(_h) + "\t" + str(_i) + "\t" + str(_j))
+        a.autonMove(b,c,d,e,f,g,_h,_i, _j)
