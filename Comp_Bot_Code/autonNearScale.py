d6928ad9d6eb4a9d635b8c8780bdf7a49f17a220
Karthik Karyamapudi <karthikkaryamapudi@gmail.com>
2018-03-31 11:10:59 -0400

    removed delay st start of near scale
    untested

--- Comp_Bot_Code/autonNearScale.py
+++ Comp_Bot_Code/autonNearScale.py
@@ -5,15 +5,13 @@ from autonBaseInit import *
 class autonNearScale(autonBaseInit):
     def run(self):
         if self.side == 'left' and self.scalePosition == 'L':
-            self.drive.autonMove(1, 2, setLiftPosition = 2)
+            self.drive.autonMove(1, 0, 1, 187, 0, 0, 3, 0)
 
-            self.drive.autonMove(2, 0, 1, 187, 0, 0, 3, 0)
+            self.drive.autonMove(2, 1, 0, 0, 22, .75, 3, 0)
 
-            self.drive.autonMove(3, 1, 0, 0, 22, .75, 3, 0)
+            self.drive.autonMove(3, 2, setLiftPosition = 3, intakeMode = 0)
 
-            self.drive.autonMove(4, 2, setLiftPosition = 3, intakeMode = 0)
-
-            self.drive.autonMove(5, 3, 0, 0, 0, 0, 3, 3)
+            self.drive.autonMove(4, 3, 0, 0, 0, 0, 3, 3)
 
             #self.drive.autonMove(6, 1, turnAngle = 90, turnSpeed = .75, setLiftPosition = 3, intakeMode = 0)
 
@@ -22,15 +20,13 @@ class autonNearScale(autonBaseInit):
             #self.drive.autonMove(8, 2, setLiftPosition = 0)
 
         if self.side == 'right' and self.scalePosition == 'R':
-            self.drive.autonMove(1, 2, setLiftPosition = 2)
+            self.drive.autonMove(1, 0, 1, 187, 0, 0, 3, 0)
 
-            self.drive.autonMove(2, 0, 1, 187, 0, 0, 3, 0)
+            self.drive.autonMove(2, 1, 0, 0, -28, .75, 3, 0)
 
-            self.drive.autonMove(3, 1, 0, 0, -28, .75, 3, 0)
+            self.drive.autonMove(3, 2, setLiftPosition = 3, intakeMode = 0)
 
-            self.drive.autonMove(4, 2, setLiftPosition = 3, intakeMode = 0)
-
-            self.drive.autonMove(5, 3, 0, 0, 0, 0, 3, 3)
+            self.drive.autonMove(4, 3, 0, 0, 0, 0, 3, 3)
 
             #self.drive.autonMove(6, 1, turnAngle = -90, turnSpeed = .75, setLiftPosition = 3, intakeMode = 0)
 
