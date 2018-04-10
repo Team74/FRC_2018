9bbf048f350e577e4bc9ba1f2de6d91477746828
Sam VanderArk <vanderarks@brpsk12.org>
2018-04-10 18:50:12 -0400

    Added intakes to the AutonInterpreter. Needless to say this is all untested.

--- Comp_Bot_Code/AutonInterpreter.py
+++ Comp_Bot_Code/AutonInterpreter.py
@@ -16,6 +16,9 @@ class AutonInterpreter(autonBaseInit):
         self.masterlist = []
         self.default_loc = "/home/lvuser/prog_auton_dir/" #"/home/svanderark/FRC_2018/GUI/convert/"
         with open(self.default_loc + name, "r") as f:
+            lift_pos = 0
+            intake_mode = 0
+
             data = f.readlines()
             i = 0
             del data[0] #first line is absolute position, for later re-import
@@ -24,15 +27,19 @@ class AutonInterpreter(autonBaseInit):
                 i += 1
                 temp = line[:-1].split(",")
                 if temp[0] == "0":
-                    self.masterlist.append(partial(self.bloob, self.drive, i, 0, float(temp[1]), float(temp[2]), 0, 0))
+                    self.masterlist.append(partial(self.bloob, self.drive, i, 0, float(temp[1]), float(temp[2]), 0, 0, lift_pos, intake_mode))
                 elif temp[0] == "1":
                     turnSpeed = 0.5
-                    self.masterlist.append(partial(self.bloob, self.drive, i, 1, 0, 0, float(temp[1]), float(turnSpeed)))
+                    self.masterlist.append(partial(self.bloob, self.drive, i, 1, 0, 0, float(temp[1]), float(turnSpeed), lift_pos, intake_mode))
+                elif temp[0] == "2":
+                    lift_pos = int(temp[1])
+                elif temp[0] == "3":
+                    intake_mode = int(temp[1])
                 else:
-                    self.masterlist.append(partial(self.bloob, self.drive, i, float(temp[0]), 0,0,0,0))
+                    self.masterlist.append(partial(self.bloob, self.drive, i, float(temp[0]), 0,0,0,0, lift_pos, intake_mode))
     def run(self):
         for i in self.masterlist:
             i()
 
-    def bloob(self, a, b, c, d, e, f, g, _h=0, _i=0):
-        a.autonMove(b,c,d,e,f,g,0,0)
+    def bloob(self, a, b, c, d, e, f, g, h, i):
+        a.autonMove(b,c,d,e,f,g,h,i,0)
